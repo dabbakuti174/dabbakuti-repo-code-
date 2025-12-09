60a4cc7f733c9e665a0b7a72fb4b4d6843bb1c26
dabbakuti174 <dabbakutisk22@gmail.com>
2025-12-09 23:04:35 +0530

    added tags to vnet

--- vnet.tf
+++ vnet.tf
@@ -5,4 +5,9 @@ resource "azurerm_virtual_network" "vnet1"{
     address_space = ["10.0.0.0/16"]
     resource_group_name = azurerm_resource_group.rg1.name
     location = azurerm_resource_group.rg1.location
+    
+
+    tags = {
+    environment = "dev"
+  }
 }
