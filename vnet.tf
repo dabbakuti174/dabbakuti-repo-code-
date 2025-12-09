73b067f9d2abe4894a854ce23f675066660a61e2
dabbakuti174 <dabbakutisk22@gmail.com>
2025-12-09 22:54:29 +0530

    added vnet code

---
+++ vnet.tf
@@ -0,0 +1,8 @@
+
+
+resource "azurerm_virtual_network" "vnet1"{
+    name = "vnet-1"
+    address_space = ["10.0.0.0/16"]
+    resource_group_name = azurerm_resource_group.rg1.name
+    location = azurerm_resource_group.rg1.location
+}
