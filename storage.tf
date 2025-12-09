5153ddbe9e589cddeaebe933d0a1f85a3aa37060
dabbakuti174 <dabbakutisk22@gmail.com>
2025-12-09 22:56:47 +0530

    added storage code

---
+++ storage.tf
@@ -0,0 +1,13 @@
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "srikanth31589"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
